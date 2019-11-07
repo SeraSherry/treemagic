44f3908f005ed8945018d429ebdeeee7553b420a
SeraSherry <39589780+SeraSherry@users.noreply.github.com>
2019-11-07 20:26:36 +0800

    Add Deluxe System

---
+++ src/main/java/com/github/SeraSherry/treemagic/item/ItemDeluxeAppleSeed.java
@@ -0,0 +1,13 @@
+package com.github.SeraSherry.treemagic.item;
+
+import net.minecraft.item.Item;
+import com.github.SeraSherry.treemagic.creativetab.CreativeTabsLoader;
+
+public class ItemDeluxeAppleSeed extends Item{
+    public ItemDeluxeAppleSeed()
+    {
+        super();
+        this.setUnlocalizedName("deluxeAppleSeed");
+        this.setCreativeTab(CreativeTabsLoader.tabTreeMagic);
+    }
+}
